db83d447c8024350c2b7eee44b060db4590f8249
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-07-31 16:51:27 +0530

    discard: home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,9 @@
-import Logo from '@/components/Logo'
 import React from 'react'
-import Sidebar from './Sidebar'
-import RightPanel from './RightPanel'
 
-const page = () => {
+const Home = () => {
   return (
-    <div className='bg-card'>
-      <div className='max-w-350 mx-auto min-h-dvh flex gap-1 py-1'>
-        {/* Left Panel */}
-        <div className='w-60 bg-card h-full'>
-          <div className='w-30 p-5'>
-            <Logo />
-          </div>
-          <Sidebar />
-        </div>
-
-        {/* Main */}
-        <div className='flex-1 bg-background rounded-xl'></div>
-
-        {/* Right Panel */}
-        <div className='w-100'>
-          <RightPanel />
-        </div>
-      </div>
-    </div>
+    <div className='w-full min-h-dvh flex items-center justify-center'>Home</div>
   )
 }
 
-export default page
+export default Home
