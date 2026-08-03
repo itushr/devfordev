51272fcff1f28f42ae0fd3902b1328d0142f4c8c
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-08-03 23:12:31 +0530

    style: tabs/home/rightpanel

--- app/(tabs)/HomeTab.tsx
+++ app/(tabs)/HomeTab.tsx
@@ -2,7 +2,7 @@ import Pitch from "@/components/Pitch"
 
 const HomeTab = () => {
   return (
-    <div className='w-full max-w-275 mx-auto h-fit flex justify-between'>
+    <div className='w-full max-w-260 mx-auto h-fit flex justify-between'>
       <div className="w-150 py-15 border">
         <Pitch />
         <Pitch />
@@ -27,8 +27,15 @@ const HomeTab = () => {
         <Pitch />
         <Pitch />
       </div>
-      <div className="w-115 border-x py-15">
-        <div className="h-140 border-y p-2" style={{
+      <div className="w-100 border-x py-15">
+        <div className="h-120 border-y p-2" style={{
+          backgroundColor: 'var(--background)',
+          backgroundImage: `repeating-linear-gradient(-45deg, transparent, transparent 4px, var(--border) 4px, var(--border) 5px)`,
+        }}>
+          <div className="h-full w-full bg-background rounded-2xl"></div>
+        </div>
+
+        <div className="h-120 mt-2 border-y p-2" style={{
           backgroundColor: 'var(--background)',
           backgroundImage: `repeating-linear-gradient(-45deg, transparent, transparent 4px, var(--border) 4px, var(--border) 5px)`,
         }}>
