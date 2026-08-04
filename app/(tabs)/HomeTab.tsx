641006850238861ae01d0056fce9323cd8b89155
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-08-04 18:50:25 +0530

    feat(ui): search

--- app/(tabs)/HomeTab.tsx
+++ app/(tabs)/HomeTab.tsx
@@ -1,4 +1,5 @@
 import Pitch from "@/components/Pitch"
+import { Search } from "lucide-react"
 
 const HomeTab = () => {
   return (
@@ -27,7 +28,15 @@ const HomeTab = () => {
         <Pitch />
         <Pitch />
       </div>
-      <div className="w-100 border-x py-15">
+      <div className="w-100 border-x">
+        <div className="h-15 flex items-center px-5">
+          <div className="flex items-center border px-5 py-2 rounded-full gap-2 w-full">
+            <div>
+              <Search size={15} />
+            </div>
+            <input placeholder="search" className="w-full outline-none" />
+          </div>
+        </div>
         <div className="h-120 border-y p-2" style={{
           backgroundColor: 'var(--background)',
           backgroundImage: `repeating-linear-gradient(-45deg, transparent, transparent 4px, var(--border) 4px, var(--border) 5px)`,
