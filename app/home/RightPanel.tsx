9910a4af21fe0a4bb482a0999154de69c9dc211d
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-08-05 00:33:05 +0530

    refactor: ui fix

--- app/home/RightPanel.tsx
+++ app/home/RightPanel.tsx
@@ -1,8 +1,9 @@
+import Separator from '@/components/Separator'
 import { Search } from 'lucide-react'
 
 const RightPanel = () => {
     return (
-        <div className="w-100 border-x">
+        <div className="w-100 border-x sticky top-0 h-dvh">
             <div className="h-15 flex items-center px-5">
                 <div className="flex items-center border px-5 py-2 rounded-full gap-2 w-full">
                     <div>
@@ -11,19 +12,16 @@ const RightPanel = () => {
                     <input placeholder="search" className="w-full outline-none" />
                 </div>
             </div>
-            <div className="h-120 border-y p-2" style={{
-                backgroundColor: 'var(--background)',
-                backgroundImage: `repeating-linear-gradient(-45deg, transparent, transparent 4px, var(--border) 4px, var(--border) 5px)`,
-            }}>
+            
+            <div className="h-100 border-y p-2">
                 <div className="h-full w-full bg-background rounded-2xl"></div>
             </div>
+            <Separator />
 
-            <div className="h-120 mt-2 border-y p-2" style={{
-                backgroundColor: 'var(--background)',
-                backgroundImage: `repeating-linear-gradient(-45deg, transparent, transparent 4px, var(--border) 4px, var(--border) 5px)`,
-            }}>
+            <div className="h-100 border-y p-2">
                 <div className="h-full w-full bg-background rounded-2xl"></div>
             </div>
+            <Separator />
         </div>
     )
 }
