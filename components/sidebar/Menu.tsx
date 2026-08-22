00d0cd4fc6483d88f1f4c459d294907ff30c2a70
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-08-22 21:10:24 +0530

    refactor(ui): sidebar -> terminal style

--- components/sidebar/Menu.tsx
+++ components/sidebar/Menu.tsx
@@ -8,12 +8,21 @@ const Menu = ({ active }: { active: string }) => {
             {
                 menuOptions.map((item, index) => (
                     <Link key={index} href={item.link} >
-                        <div className="py-2 px-7 flex relative gap-2 font-mono tracking-wide hover:text-foreground cursor-pointer font-semibold">
-                            {item.text == active &&
-                                <div className="absolute left-0">
-                                    <ChevronRight />
-                                </div>}
-                            {item.text}
+                        <div className="py-2 px-7 flex relative gap-2 font-mono tracking-wide hover:text-foreground cursor-pointer text-lg">
+                            {item.text == active ?
+                                <>
+                                    <div className="absolute left-0">
+                                        <ChevronRight />
+                                    </div>
+                                    <span className="lowercase">
+                                        ~/{item.text}
+                                    </span>
+                                </>
+                                :
+                                <span className="lowercase text-foreground/50 hover:text-foreground">
+                                    ~/{item.text}
+                                </span>
+                            }
                         </div>
                     </Link>
                 ))
