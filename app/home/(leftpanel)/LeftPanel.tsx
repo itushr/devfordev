f3e2ff98b7753ec51ee80d22d89c680a43384d68
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-15 16:42:33 +0530

    restyle: feed header

--- app/home/(leftpanel)/LeftPanel.tsx
+++ app/home/(leftpanel)/LeftPanel.tsx
@@ -2,24 +2,18 @@ import Feed from "./Feed"
 import { Roboto } from "next/font/google"
 import PitchComposer from "@/components/pitchComposer/PitchComposer"
 import Separator from "@/components/Separator"
-import { ChevronRight } from "lucide-react"
+import { ChevronDown, ChevronRight } from "lucide-react"
 
 const LeftPanel = () => {
     return (
         <div className="w-150 border pt-15">
-            <div className={`z-10 h-15 w-149.5 bg-background/20 backdrop-blur-2xl border-b flex items-end px-5 gap-10 tracking-wide fixed top-0 text-md font-mono`}>
-                <div className="pb-3 flex items-center gap-1 relative">
-                    <ChevronRight size={15} />
-                    For You
+            <div className={`z-10 h-15 w-149.5 bg-background/20 backdrop-blur-2xl border-b flex justify-between items-center px-5 gap-10 tracking-wide fixed top-0 text-md font-mono`}>
+                <div className="h-15 font-mono flex items-center text-foreground/50">
+                    home@devfordev ~ %
                 </div>
-                <div className="pb-3 text-muted-foreground">
-                    Network
-                </div>
-                <div className="pb-3 text-muted-foreground">
-                    Need Attention
-                </div>
-                <div className="pb-3 text-muted-foreground">
-                    Recent
+                <div className="flex items-center gap-1 text-foreground/70">
+                    <span>for you</span>
+                    <ChevronDown size={15} />
                 </div>
             </div>
             {/* <PitchComposer />
