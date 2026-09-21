6a0fdfe8376f42e1f36b06898f342a1152deb82a
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-09-21 23:06:48 +0530

    Revert "refactor: show userdata at sidebar user profile"
    This reverts commit 93c93518d967108d6a004ebde4cc1f33ea0a2398.

--- components/sidebar/Profile.tsx
+++ components/sidebar/Profile.tsx
@@ -1,16 +1,13 @@
 import { MoreHorizontal } from 'lucide-react'
 import Avatar from '../Avatar'
-import { useAuthStore } from '@/store/auth'
 
 const Profile = () => {
-    const { authUser } = useAuthStore();
-
     return (
         <div className="border-t w-full px-5 py-3 flex gap-3 items-center">
             <Avatar image="/random-pfps/pfp5.jpeg" />
             
             <div className="flex-1 flex flex-col justify-center">
-                <p>{authUser?.role}</p>
+                <p>Tushar</p>
                 <p className="text-foreground/70">@iamtushar</p>
             </div>
             <div className="hover:bg-card p-2 rounded-full cursor-pointer">
